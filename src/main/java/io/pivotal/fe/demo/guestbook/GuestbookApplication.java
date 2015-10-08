25f1289d42520a7c78ac02564ee56f9c26dea3ff
jaimegag <jaguilar@pivotal.io>
2015-10-08 18:46:20 -0400

    Use of eureka, ribbon, hystrix and integration with fortune service

--- src/main/java/io/pivotal/fe/demo/guestbook/GuestbookApplication.java
+++ src/main/java/io/pivotal/fe/demo/guestbook/GuestbookApplication.java
@@ -3,9 +3,13 @@ package io.pivotal.fe.demo.guestbook;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableAutoConfiguration
+@EnableCircuitBreaker
+@EnableDiscoveryClient
 public class GuestbookApplication {
 
     public static void main(String[] args) {
